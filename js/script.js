20acd3779c20eaca3e78cf29fb0a84c52da57e41
hisanori <wwshimabara@gmail.com>
2021-07-22 07:05:42 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // step表示の際に、クリックしたエレメントにclassを付加する。
-document.getElementById('step','step2','step3','step4','step5','step6','step7').addEventListener('click',function(){
+document.getElementById('step1','step2','step3','step4','step5','step6','step7').addEventListener('click',function(){
   this.classList.add('active');
 });
