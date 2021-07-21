caceab3722591589346cbce8805d7e39c3719be6
hisanori <wwshimabara@gmail.com>
2021-07-22 06:09:35 +0900

    add(script を書いてみた)
    classを与えるスクリプトをかいた。

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-// リロード時
-$(function() {
-  // ウィンドウサイズ768px以下の場合、クラスを削除
-  if (window.matchMedia( '(max-width: 768px)' ).matches) {
-  $(function(){
-    $('.step-7').removeClass('col-5').addClass('col-12');
-  });
-  //768px以上の場合なにもしない
-    } else {
-  };
-
-  $(window).resize(function(){
-  let x = $(window).width();
-  let y = 768;
-  if (x <= y) {
-    $('.step-7').removeClass('col-5');
-  }
-  else {
-    $('.step-7').addClass('col-12');
-  }
-});
-
+document.getElementById('step').addEventListener('click',function(){
+  document.getElementById('step').innerHTML = 'STEP';
+  this.classList.add('active');
 });
