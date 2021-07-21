76271e9d006e0f9ddd209088ad9811d6366c5244
hisanori <wwshimabara@gmail.com>
2021-07-22 07:02:29 +0900

    add(step idの追加)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // step表示の際に、クリックしたエレメントにclassを付加する。
-document.getElementById('step').addEventListener('click',function(){
+document.getElementById('step','step2','step3','step4','step5','step6','step7').addEventListener('click',function(){
   this.classList.add('active');
 });
