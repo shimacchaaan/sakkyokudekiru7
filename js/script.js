761a87c279b1e4de7ab0800f045d23955e932a39
hisanori <wwshimabara@gmail.com>
2021-07-25 05:08:35 +0900

    fix(if )

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
 // step表示の際に、クリックしたエレメントにclassを付加する。
 document.getElementById('step1').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step1').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step2').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step2').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step3').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step3').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step4').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step4').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step5').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step5').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step6').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step6').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
 
 document.getElementById('step7').addEventListener('click',function(){
-  this.classList.add('active');
+  if(document.getElementById('step7').click == true){
+    this.classList.add('active');
+  }else{
+    this.classList.remove('active')
+  }
 });
