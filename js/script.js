e4b14a88c1def598c3f8f4292e422823d4b54c97
hisanori <wwshimabara@gmail.com>
2021-07-25 05:36:41 +0900

    fix(冗長だけど、ためしに Javascript)

--- js/script.js
+++ js/script.js
@@ -1,56 +1,70 @@
 // step表示の際に、クリックしたエレメントにclassを付加する。
 document.getElementById('step1').addEventListener('click',function(){
-  if(document.getElementById('step1').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step6').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step2').addEventListener('click',function(){
-  if(document.getElementById('step2').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step6').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step3').addEventListener('click',function(){
-  if(document.getElementById('step3').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step6').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step4').addEventListener('click',function(){
-  if(document.getElementById('step4').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step6').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step5').addEventListener('click',function(){
-  if(document.getElementById('step5').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step6').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step6').addEventListener('click',function(){
-  if(document.getElementById('step6').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step7').removeClass('active');
 });
 
 document.getElementById('step7').addEventListener('click',function(){
-  if(document.getElementById('step7').click == true){
-    this.classList.add('active');
-  }else{
-    this.classList.remove('active')
-  }
+  this.classList.add('active');
+  getElementById('step1').removeClass('active');
+  getElementById('step2').removeClass('active');
+  getElementById('step3').removeClass('active');
+  getElementById('step4').removeClass('active');
+  getElementById('step5').removeClass('active');
+  getElementById('step6').removeClass('active');
 });
